c4f057b8ca0fa8b8fbc4841136a1d406919cb40b
cpatuasic <cpatuasic@yondu.com>
2017-02-06 17:08:48 +0800

    Added Test content

---
+++ resources/views/me.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+            me lang
+        </title>
+    </head>
+    <body>
+
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,9 @@
             html, body {
                 height: 100%;
             }
+            @font-face {
 
+            }
             body {
                 margin: 0;
                 padding: 0;
@@ -17,6 +19,7 @@
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato';
+                color: red;
             }
 
             .container {
@@ -33,12 +36,54 @@
             .title {
                 font-size: 96px;
             }
+            body{
+                background-color: #171616;
+            }
+            ul{
+
+                list-style: none;
+            }
+            p{
+                color: #fff;
+                font-family: myFirstFont;
+              -webkit-animation: neon1 1.5s ease-in-out infinite alternate;
+              -moz-animation: neon1 1.5s ease-in-out infinite alternate;
+              animation: neon1 1.5s ease-in-out infinite alternate;
+            }
+
+
+            @-webkit-keyframes neon1 {
+              from {
+                text-shadow: 0 0 20px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #FF1177, 0 0 70px #FF1177, 0 0 80px #FF1177, 0 0 100px #FF1177, 0 0 150px #FF1177;
+              }
+              to {
+                text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #FF1177, 0 0 35px #FF1177, 0 0 40px #FF1177, 0 0 50px #FF1177, 0 0 75px #FF1177;
+              }
+            }
+
+            @-webkit-keyframes neon2 {
+              from {
+                text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #228DFF, 0 0 70px #228DFF, 0 0 80px #228DFF, 0 0 100px #228DFF, 0 0 150px #228DFF;
+              }
+              to {
+                text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #228DFF, 0 0 35px #228DFF, 0 0 40px #228DFF, 0 0 50px #228DFF, 0 0 75px #228DFF;
+              }
+            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Projects</li>
+                        <li>About Us</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+                <div class="title"><p>Me</p></div>
+
             </div>
         </div>
     </body>
